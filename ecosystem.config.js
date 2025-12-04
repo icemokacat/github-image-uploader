1626119a33ababfc192e0a0dfaf9fee79927c2ca
모카 <icemoka@kakao.com>
2025-12-04 14:34:56 +0900

    운영환경 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,15 +5,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
+    max_memory_restart: '300M',
     error_file: './logs/err.log',
     out_file: './logs/out.log',
     log_file: './logs/combined.log',
